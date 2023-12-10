3ef1730b12841899331ca522bc29550275cd2d38
archana444ku <123111021+archana444ku@users.noreply.github.com>
2023-12-10 12:52:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showFireworks() {
+    // You can add code here to display fireworks or any other animation
+    // For simplicity, let's just change the background color after 2 seconds
+    setTimeout(() => {
+        document.body.style.backgroundColor = '#111';
+    }, 2000);
+}
